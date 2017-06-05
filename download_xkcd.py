d27b681de52da247482d128d7289ca7f3182a3c3
AP Pranav <pranavap96@gmail.com>
2017-06-05 16:52:40 +0530

    automated downloader for xkcd comics

---
+++ download_xkcd.py
@@ -0,0 +1,38 @@
+# Downloads every single XKCD comic.
+
+import requests, os, bs4
+
+url = 'http://xkcd.com'
+os.makedirs('xkcd', exist_ok=True)
+
+while not url.endswith('#'):
+    # Download the page.
+    print('Downloading page %s...' % url)
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+    # Find the URL of the comic image.
+    comic_elem = soup.select('#comic img')
+    if comic_elem is []:
+        print('Could not find comic image.')
+    else:
+        comic_url = comic_elem[0].get('src')
+        print('Downloading image %s...' % comic_url)
+        res = requests.get('http:' + comic_url)
+        res.raise_for_status()
+
+        # Save the image to ./xkcd
+        image_file = open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb')
+
+        for chunk in res.iter_content(100000):
+            image_file.write(chunk)
+
+        image_file.close()
+
+    # Get the Prev button's url.
+    prev_link = soup.select('a[rel="prev"]')[0]
+    url = 'http://xkcd.com' + prev_link.get('href')
+
+print('Done.')

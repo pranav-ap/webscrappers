aa4641a782db4737b0028ec132260e774a0d26e5
AP Pranav <pranavap96@gmail.com>
2017-08-29 20:00:25 +0530

    refactoring done

--- wikipedia_word_frequency.py
+++ wikipedia_word_frequency.py
@@ -12,45 +12,38 @@ from stop_words import get_stop_words
 
 def getWordList(url):
     word_list = []
-    #raw data
+    # raw data
     source_code = requests.get(url)
-    #convert to text
+    # convert to text
     plain_text = source_code.text
-    #lxml format
-    soup = BeautifulSoup(plain_text,'lxml')
+    # lxml format
+    soup = BeautifulSoup(plain_text, 'lxml')
 
-    #find the words in paragraph tag
-    for text in soup.findAll('p'):
-        if text.text is None:
+    # find the words in paragraph tag
+    for element in soup.findAll('p'):
+        if element.text is None:
             continue
-        #content
-        content = text.text
-        #lowercase and split into an array
+
+        content = element.text
         words = content.lower().split()
 
-        #for each word
         for word in words:
-            #remove non-chars
             cleaned_word = clean_word(word)
-            #if there is still something there
             if len(cleaned_word) > 0:
-                #add it to our word list
                 word_list.append(cleaned_word)
 
     return word_list
 
 
-#clean word with regex
 def clean_word(word):
     cleaned_word = re.sub('[^A-Za-z]+', '', word)
     return cleaned_word
 
 
 def createFrquencyTable(word_list):
-    #word count
     word_count = {}
+
     for word in word_list:
-        #index is the word
         if word in word_count:
             word_count[word] += 1
         else:
@@ -58,75 +51,81 @@ def createFrquencyTable(word_list):
 
     return word_count
 
-#remove stop words
+
 def remove_stop_words(frequency_list):
     stop_words = get_stop_words('en')
 
     temp_list = []
-    for key,value in frequency_list:
+    for key, value in frequency_list:
         if key not in stop_words:
             temp_list.append([key, value])
 
     return temp_list
 
-wikipedia_api_link = "https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch="
-wikipedia_link = "https://en.wikipedia.org/wiki/"
+def main():
+    wikipedia_api_link = "https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srsearch="
+    wikipedia_link = "https://en.wikipedia.org/wiki/"
 
-if len(sys.argv) < 2:
-    print("Enter valid string")
-    exit()
+    if len(sys.argv) < 2:
+        print("Enter valid string")
+        exit()
 
-string_query = sys.argv[1]
+    string_query = sys.argv[1]
 
-remove = False
-if len(sys.argv) > 2 and sys.argv[2] == 'yes':
-  remove = True    
+    remove = False
+    if len(sys.argv) > 2 and sys.argv[2] == 'yes':
+        remove = True    
 
-url = wikipedia_api_link + string_query
+    url = wikipedia_api_link + string_query
 
-try:
-    response = requests.get(url)
-    # print(response.content)
-    data = json.loads(response.content.decode("utf-8"))
-    # print(data)
+    try:
+        response = requests.get(url)
+        # print(response.content)
+        data = json.loads(response.content.decode("utf-8"))
+        # print(data)
 
-    # select the first page result
-    wikipedia_page_tag = data['query']['search'][0]['title']
-    url = wikipedia_link + wikipedia_page_tag
+        # select the first page result
+        wikipedia_page_tag = data['query']['search'][0]['title']
+        url = wikipedia_link + wikipedia_page_tag
+        url = re.sub('\s+', '_', url)
+        print('The page analysed is : ', url)
 
-    # get list of words from that page
-    page_word_list = getWordList(url)
+        # get list of words from that page
+        page_word_list = getWordList(url)
 
-except requests.exceptions.Timeout:
-    print("The server didn't respond. Please, try again later.")
+    except requests.exceptions.Timeout:
+        print("The server didn't respond. Please, try again later.")
 
-# create table of word counts, dictionary
-page_word_count = createFrquencyTable(page_word_list)
+    # create table of word counts, dictionary
+    page_word_count = createFrquencyTable(page_word_list)
 
-# sort the table by the frequency count
-sorted_word_frequency_list = sorted(page_word_count.items(), key=operator.itemgetter(1), reverse=True)
+    # sort the table by the frequency count
+    sorted_word_frequency_list = sorted(page_word_count.items(), key=operator.itemgetter(1), reverse=True)
 
-# remove stop words if the user specified
-if remove:
-    sorted_word_frequency_list = remove_stop_words(sorted_word_frequency_list)
+    # remove stop words if the user specified
+    if remove:
+        sorted_word_frequency_list = remove_stop_words(sorted_word_frequency_list)
 
-# sum the total words to calculate frequencies   
-total_words_sum = 0
-for key, value in sorted_word_frequency_list:
-    total_words_sum = total_words_sum + value
+    # sum the total words to calculate frequencies   
+    total_words_sum = 0
+    for key, value in sorted_word_frequency_list:
+        total_words_sum = total_words_sum + value
 
-# just get the top 20 words
-if len(sorted_word_frequency_list) > 20:
-    sorted_word_frequency_list = sorted_word_frequency_list[:20]
+    # just get the top 20 words
+    if len(sorted_word_frequency_list) > 20:
+        sorted_word_frequency_list = sorted_word_frequency_list[:20]
 
-# create our final list which contains words, frequency (word count), percentage
-final_list = []
-for key, value in sorted_word_frequency_list:
-    percentage_value = float(value * 100) / total_words_sum
-    final_list.append([key, value, round(percentage_value, 4)])
+    # create our final list which contains words, frequency (word count), percentage
+    final_list = []
+    for key, value in sorted_word_frequency_list:
+        percentage_value = float(value * 100) / total_words_sum
+        final_list.append([key, value, round(percentage_value, 4)])
 
-# headers before the table
-print_headers = ['Word', 'Frequency', 'Frequency Percentage']
+    # headers before the table
+    print_headers = ['Word', 'Frequency', 'Frequency Percentage']
 
-#print the table with tabulate
-print(tabulate(final_list, headers=print_headers, tablefmt='orgtbl'))
+    #print the table with tabulate
+    print(tabulate(final_list, headers=print_headers, tablefmt='orgtbl'))
+
+if __name__ == '__main__':
+    main()

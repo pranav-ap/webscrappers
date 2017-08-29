878da8529fcca792377c4e96c3c49808b7e4dcef
AP Pranav <pranavap96@gmail.com>
2017-08-29 17:34:09 +0530

    some refactoring

--- wikipedia_word_frequency.py
+++ wikipedia_word_frequency.py
@@ -78,62 +78,55 @@ if len(sys.argv) < 2:
 
 string_query = sys.argv[1]
 
-search_mode = False
-#to remove stop words or not
-if len(sys.argv) > 2 and sys.argv[2] === 'yes':
-  search_mode = True    
+remove = False
+if len(sys.argv) > 2 and sys.argv[2] == 'yes':
+  remove = True    
 
-#create our URL
 url = wikipedia_api_link + string_query
 
-#try-except block. simple way to deal with exceptions 
-#great for HTTP requests
 try:
-    #use requests to retrieve raw data from wiki API URL we
-    #just constructed
     response = requests.get(url)
-
-    #format that data as a JSON dictionary
+    # print(response.content)
     data = json.loads(response.content.decode("utf-8"))
+    # print(data)
 
-    #page title, first option
-    #show this in web browser
+    # select the first page result
     wikipedia_page_tag = data['query']['search'][0]['title']
-
-    #get actual wiki page based on retrieved title
     url = wikipedia_link + wikipedia_page_tag
-    #get list of words from that page
+
+    # get list of words from that page
     page_word_list = getWordList(url)
-    #create table of word counts, dictionary
-    page_word_count = createFrquencyTable(page_word_list)
-    #sort the table by the frequency count
-    sorted_word_frequency_list = sorted(page_word_count.items(), key=operator.itemgetter(1), reverse=True)
-    #remove stop words if the user specified
-    if(search_mode):
-        sorted_word_frequency_list = remove_stop_words(sorted_word_frequency_list)
 
-    #sum the total words to calculate frequencies   
-    total_words_sum = 0
-    for key,value in sorted_word_frequency_list:
-        total_words_sum = total_words_sum + value
-
-    #just get the top 20 words
-    if len(sorted_word_frequency_list) > 20:
-        sorted_word_frequency_list = sorted_word_frequency_list[:20]
-
-    #create our final list which contains words, frequency (word count), percentage
-    final_list = []
-    for key,value in sorted_word_frequency_list:
-        percentage_value = float(value * 100) / total_words_sum
-        final_list.append([key, value, round(percentage_value, 4)])
-
-    #headers before the table
-    print_headers = ['Word', 'Frequency', 'Frequency Percentage']
-
-    #print the table with tabulate
-    print(tabulate(final_list, headers=print_headers, tablefmt='orgtbl'))
-
-#throw an exception in case it breaks
 except requests.exceptions.Timeout:
     print("The server didn't respond. Please, try again later.")
 
+# create table of word counts, dictionary
+page_word_count = createFrquencyTable(page_word_list)
+
+# sort the table by the frequency count
+sorted_word_frequency_list = sorted(page_word_count.items(), key=operator.itemgetter(1), reverse=True)
+
+# remove stop words if the user specified
+if remove:
+    sorted_word_frequency_list = remove_stop_words(sorted_word_frequency_list)
+
+# sum the total words to calculate frequencies   
+total_words_sum = 0
+for key, value in sorted_word_frequency_list:
+    total_words_sum = total_words_sum + value
+
+# just get the top 20 words
+if len(sorted_word_frequency_list) > 20:
+    sorted_word_frequency_list = sorted_word_frequency_list[:20]
+
+# create our final list which contains words, frequency (word count), percentage
+final_list = []
+for key, value in sorted_word_frequency_list:
+    percentage_value = float(value * 100) / total_words_sum
+    final_list.append([key, value, round(percentage_value, 4)])
+
+# headers before the table
+print_headers = ['Word', 'Frequency', 'Frequency Percentage']
+
+#print the table with tabulate
+print(tabulate(final_list, headers=print_headers, tablefmt='orgtbl'))

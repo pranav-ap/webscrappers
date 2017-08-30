b5c3b0909d5456942fbb0d34cd57162d889c5a1b
AP Pranav <pranavap96@gmail.com>
2017-08-30 09:33:52 +0530

    minor change

--- email_extractor.py
+++ email_extractor.py
@@ -1,7 +1,10 @@
+#extracts all email addresses from the webpage
+
 from bs4 import BeautifulSoup
 from pprint import pprint
 import re
 import requests
+
 
 def extract(url):
     emails = []
@@ -15,10 +18,6 @@ def extract(url):
     soup = BeautifulSoup(data, 'lxml')
     # print(soup.encode("utf-8"))
     emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", soup.encode("utf-8").decode("utf-8"))
-
-    # for link in soup.find_all('a'):
-    #     if link.has_attr('href'):
-    #         emails.append(link.get('href'))
     
     return emails
 

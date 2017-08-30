ca23343d31505c2e3a08f3dfca935e3e8ad4b03a
AP Pranav <pranavap96@gmail.com>
2017-08-30 06:47:57 +0530

    refactored to use function

--- url_extractor.py
+++ url_extractor.py
@@ -1,17 +1,28 @@
 from bs4 import BeautifulSoup
 import requests
 
-try:
-  url = 'https://en.wikipedia.org/wiki/Main_Page'
+def extract(url):
+    links = []
+    try:
+        response  = requests.get(url)
+        data = response.text
 
-  response  = requests.get(url)
-  data = response.text
+    except requests.exceptions.Timeout:
+        print("The server didn't respond. Please, try again later.")
 
-except requests.exceptions.Timeout:
-    print("The server didn't respond. Please, try again later.")
+    soup = BeautifulSoup(data, 'lxml')
 
-soup = BeautifulSoup(data, 'lxml')
+    for link in soup.find_all('a'):
+        if link.has_attr('href'):
+            links.append(link.get('href'))
+    
+    print(links)
 
-for link in soup.find_all('a'):
-    if link.has_attr('href'):
-        print(link.get('href'))
+
+def main():
+    url = 'https://en.wikipedia.org/wiki/Main_Page'
+    extract(url)
+
+
+if __name__ == '__main__':
+    main()
